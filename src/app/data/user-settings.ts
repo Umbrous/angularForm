9d50dfce530a9920fd9b30355d933b9d764f2575
Andrey Kozyrev <kozurevav@gmail.com>
2019-10-17 23:08:53 +0300

    valided form & field

---
+++ src/app/data/user-settings.ts
@@ -0,0 +1,7 @@
+export interface UserSettings {
+    name: string,
+    emailOffers: boolean,
+    interfaceStyle: string,
+    subscritionType: string,
+    notes: string
+}
--- src/app/user-setting-form/user-setting-form.component.ts
+++ src/app/user-setting-form/user-setting-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-setting-form',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingFormComponent implements OnInit {
 
+  OriginalUserSettings: UserSettings = {
+     name: null,
+     emailOffers: null,
+     interfaceStyle: null,
+     subscritionType: null,
+     notes: null
+  };
+
+  userSettings: UserSettings = { ...this.OriginalUserSettings }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
   }
 
 }
